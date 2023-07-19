b2cc4a0b439ebc2fbd2e5acee588830008a36c25
Rahil <rahil393@gmail.com>
2023-07-19 13:50:15 +0530

    update files

--- JavaScript/script_Lclass1.js
+++ JavaScript/script_Lclass1.js
@@ -2,5 +2,5 @@ document.write("addition:- ")
 document.write("34+67=")
 document.write(34 + 67);
 
-// to declare a varibale we can use and inbuilt keywork called var
+// to declare a varibale we can use and inbuilt keyword called var
 //  var fullname=  "Rahil khan" //syntax
