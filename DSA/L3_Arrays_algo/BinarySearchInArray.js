5a5b756c0c871c6b6d4f1a96c94d39274677351c
krahil1996 <rahil393@gmail.com>
2023-09-27 18:32:20 +0530

    update files

---
+++ DSA/L3_Arrays_algo/BinarySearchInArray.js
@@ -0,0 +1,20 @@
+var arr = [2, 8, 11, 15, 22, 32, 47, 48, 51, 66, 72];
+function binarySearch(arr, target) {
+    var l = 0;
+    var r = arr.length - 1;
+    while (l <= r) {
+        var mid = Math.floor((l + r) / 2);
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target) {
+            // Discard left half, as i am smaller, so all the people to my left side also be smaller.
+            l = mid + 1;
+        }
+        else if (arr[mid] > target) {
+            // Discard right half, as i am greater, so all the people to my right side also be greater.
+            r = mid - 1;
+        }
+    }
+    return "Element is Not Found in the Array"; // Element is not present in the array. 
+}
+console.log(binarySearch(arr, 22));
