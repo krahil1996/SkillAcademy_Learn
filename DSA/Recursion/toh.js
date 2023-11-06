61fd3d00dd197e5a019a2aaa94cbefe1cd7da415
krahil1996 <rahil393@gmail.com>
2023-11-06 19:00:48 +0530

    update files

---
+++ DSA/Recursion/toh.js
@@ -0,0 +1,8 @@
+function toh(n, s, d, h) {
+    if (n == 0)
+        return;
+    toh(n - 1, s, h, d);
+    console.log("move disk ".concat(n, " from ").concat(s, " to ").concat(d));
+    toh(n - 1, h, d, s);
+}
+toh(3, "a", "b", "c");
---
+++ DSA/Recursion/toh.ts
@@ -0,0 +1,7 @@
+function toh (n, s ,d, h) {
+if (n==0) return ;
+toh (n-1, s, h, d);
+console.log(`move disk ${n} from ${s} to ${d}`);
+toh (n-1,h,d,s);
+}
+toh (3, "a", "b", "c");
