0cf6c50d39a270f98256250dc5d43b646187e411
krahil1996 <rahil393@gmail.com>
2023-11-08 12:44:19 +0530

    update files

---
+++ DSA/Recursion/climbStairs.js
@@ -0,0 +1,14 @@
+function climbStairs(i, psf, n) {
+    if (n == 0) {
+        console.log(psf);
+        return;
+    }
+    if (i > n) {
+        return;
+    }
+    //options
+    climbStairs(i + 1, psf + "1 ", n);
+    climbStairs(i + 2, psf + "2 ", n);
+    climbStairs(i + 3, psf + "3 ", n);
+}
+climbStairs(0, " ", 4);
