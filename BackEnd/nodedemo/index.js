fcecdc74b42e80ad1bebe3f037ed11a6cdb61e6b
krahil1996 <rahil393@gmail.com>
2023-11-29 18:46:36 +0530

    update files

---
+++ BackEnd/nodedemo/index.js
@@ -0,0 +1,3 @@
+// const simple = require('./second.mjs');
+import { simple, simple1, StudentData} from "./second.mjs";
+console.log(simple(), simple1, StudentData);
---
+++ BackEnd/nodedemo/second.mjs
@@ -0,0 +1,17 @@
+export function simple(){
+    console.log ('Hello simple')
+    return "Hello Simple Return"
+}
+export function simple1(){
+    console.log ('Hello simple1');
+    
+}
+
+const StudentData={
+    name : 'peter Parker', 
+    uid : 48,
+    designation : 'Spider-Man'
+}
+export {StudentData};
+// module.exports = StudentData
+
