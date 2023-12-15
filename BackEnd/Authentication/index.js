04e5d9476cbf952a6317d76409e60bf8a1eba1a3
krahil1996 <rahil393@gmail.com>
2023-12-15 19:08:46 +0530

    update file

--- BackEnd/Authentication/index.js
+++ BackEnd/Authentication/index.js
@@ -24,18 +24,46 @@ app.get("/", (req, res) => {
     username
   });
 });
-app.get('/login', (req,res){
-    let status = req.query.msg ? true:false;
-    if (status) {
-        return res.render('login',{
-            error: "Invalid Details"
-        })
-    }else {
-        
-    }}
-}))
+app.get ('/login', (req,res)=>{
+  let status = req.query.msg ? true : false;
+  if (status){
+    return res.render('login', {
+      error : "invalid Details"
+    })
+  }
+  else {
+    return res.render('login');
+  }
 
+}); 
 
+app.get('/welcome', (req, res) => {
+  let username = req.cookies.username;
+  return res.render('welcome', {username});
+});
+
+app.post ('/Process_login', (req,res)=>{
+  let {username, password} = req.body; // destructure
+  // let username = req.body.username
+  // let password = req.body.password
+  let userdetails = {
+    username: "john",
+    password: 'safe123',
+  };
+  if (username === userdetails['username'] && password === userdetails['password']) {
+    res.cookie('username', username);
+    return res.redirect ('/welcome');
+  }
+  else {
+    return res.redirect ('/login?msg=fail')
+  }
+})
+
+app.get ('/logout', (req,res) => {
+  res.clearCookie('username');
+  return res.redirect ('/login');
+  
+})
 
 app.listen(port, () => {
   console.log(`Server started on ${port} please goto access them.`);
